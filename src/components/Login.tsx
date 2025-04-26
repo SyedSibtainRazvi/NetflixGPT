a3d58ef6a3844ff4b7232ee177c1ab56450f96c0
SyedSibtainRazvi <syedsibtain191@gmail.com>
2025-04-27 00:39:29 +0530

    feat: :sparkles: added sign up form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import Header from './Header';
 import netflix_bg from '../assets/netflix_bg.jpg';
 
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const handleFormToggle = () => {
+    setIsSignIn(!isSignIn);
+  };
   return (
     <div className="relative min-h-screen w-full">
       <Header />
       <img src={netflix_bg} alt="netflix" className="absolute inset-0 w-full h-full object-cover" />
       <div className="absolute inset-0 flex items-center justify-center z-20 ">
-        <form className="w-96 bg-black/80 p-8 rounded-lg flex flex-col gap-10 m-4">
-          <h1 className="text-4xl font-bold text-white">Sign In</h1>
+        <form className="w-96 bg-black/80 p-8 rounded-lg flex flex-col gap-4 sm:gap-10 m-4">
+          <h1 className="text-4xl font-bold text-white">{isSignIn ? 'Sign In' : 'Sign Up'}</h1>
+          {!isSignIn && (
+            <input
+              type="text"
+              placeholder="Name"
+              className="py-4 px-4 rounded bg-black/50 text-gray-300 placeholder-gray-400 border border-gray-500 focus:outline-gray-300"
+            />
+          )}
           <input
             type="email"
             placeholder="Email or phone number"
@@ -20,10 +33,13 @@ const Login = () => {
             className="py-4 px-4 rounded bg-black/50 text-gray-300 placeholder-gray-400 border border-gray-500 focus:outline-gray-300"
           />
           <button type="submit" className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 rounded">
-            Sign In
+            {isSignIn ? 'Sign In' : 'Sign Up'}
           </button>
           <p className="text-gray-400 text-sm break-words">
-            New to Netflix? <span className="text-white">Sign up now.</span>
+            {isSignIn ? 'New to Netflix?' : 'Already have an account?'}
+            <span onClick={handleFormToggle} className="text-white cursor-pointer ml-2 hover:underline">
+              {isSignIn ? 'Sign up now.' : 'Sign in now.'}
+            </span>
           </p>
           <p className="text-gray-400 text-sm break-words">
             This page is protected by Google reCAPTCHA to ensure you're not a bot.
