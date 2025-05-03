dacb5bc4ce1f84f88b3525f09dbc84fcbaf8325c
SyedSibtainRazvi <syedsibtain191@gmail.com>
2025-05-03 17:56:49 +0530

    refactor: :zap: added lazy load section for listing

--- src/components/movie/MovieContainer.tsx
+++ src/components/movie/MovieContainer.tsx
@@ -1,21 +1,26 @@
 import MovieList from './MovieList';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../utils/appStore';
+import LazySection from '../LazySection';
 
 const MovieContainer = () => {
   const nowPlayingMovies = useSelector((store: RootState) => store.movies.nowPlayingMovies);
   const popularMovies = useSelector((store: RootState) => store.movies.popularMovies);
   const upcomingMovies = useSelector((store: RootState) => store.movies.upcomingMovies);
 
-  if (!nowPlayingMovies?.length || !popularMovies?.length || !upcomingMovies?.length) return null;
+  if (!nowPlayingMovies?.length) return null;
 
   return (
     <div className="bg-black">
       <div className="sm:-mt-32 -mt-26 relative">
         <MovieList title="Now Playing" movies={nowPlayingMovies} />
       </div>
-      <MovieList title="Popular Movies" movies={popularMovies} />
-      <MovieList title="Upcoming Movies" movies={upcomingMovies} />
+      <LazySection>
+        {popularMovies?.length ? <MovieList title="Popular Movies" movies={popularMovies} /> : null}
+      </LazySection>
+      <LazySection>
+        {upcomingMovies?.length ? <MovieList title="Upcoming Movies" movies={upcomingMovies} /> : null}
+      </LazySection>
     </div>
   );
 };
